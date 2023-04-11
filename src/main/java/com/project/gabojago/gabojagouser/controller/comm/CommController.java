3b650efa7593aebfcbabed537f0998740597a257
HIMJU <hinge1128@gmail.com>
2023-04-11 16:48:26 +0900

    230411

--- src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +24,13 @@ public class CommController {
         communities=communityService.list();
         model.addAttribute("communities",communities);
         return "/comm/list";
+    }
+
+    @GetMapping("/{cId}/detail.do")
+    public String detail(Model model,
+                         @PathVariable int cId){
+        CommunityDto comm=communityService.detail(cId);
+        model.addAttribute("c",comm);
+        return "/comm/detail";
     }
 }
