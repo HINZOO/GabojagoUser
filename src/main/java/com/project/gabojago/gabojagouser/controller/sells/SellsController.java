41ef3cb0139c35a4e17a50a80494834b386446e0
HIMJU <hinge1128@gmail.com>
2023-04-12 13:59:27 +0900

    filesetting

--- src/main/java/com/project/gabojago/gabojagouser/controller/sells/SellsController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/sells/SellsController.java
@@ -1,16 +1,14 @@
 package com.project.gabojago.gabojagouser.controller.sells;
 
 import com.project.gabojago.gabojagouser.dto.sells.SellsDto;
-import com.project.gabojago.gabojagouser.service.SellsService;
+import com.project.gabojago.gabojagouser.service.sells.SellsService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
