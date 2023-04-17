ef5a63a4dd3f14a73d47b4f634de07af42218159
yang <thddns11@naver.com>
2023-04-17 17:51:28 +0900

    qnaServiceController

--- src/main/java/com/project/gabojago/gabojagouser/controller/my/MyUserController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/my/MyUserController.java
@@ -1,14 +1,21 @@
 package com.project.gabojago.gabojagouser.controller.my;
 
+import com.project.gabojago.gabojagouser.dto.comm.CommunityDto;
+import com.project.gabojago.gabojagouser.dto.my.MyUserQnaDto;
 import com.project.gabojago.gabojagouser.dto.user.UserDto;
+import com.project.gabojago.gabojagouser.service.my.MyUserQnaService;
 import com.project.gabojago.gabojagouser.service.user.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -16,12 +23,22 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @Log4j2
 public class MyUserController {
     private UserService userService;
+    private MyUserQnaService myUserQnaService;
     @GetMapping("/user.do")
     public String list(Model model,
                        @SessionAttribute UserDto loginUser){
         model.addAttribute("user",loginUser);
         return "/my/user";
     }
-    
+    @GetMapping("/{uId}/service.do")
+    private String serviceForm(Model model,
+                       @ModelAttribute MyUserQnaDto myUserQna,
+                        @SessionAttribute UserDto loginUser){
+
+        List<MyUserQnaDto> list= myUserQnaService.list();
+        model.addAttribute("qnaList",list);
+        return "/my/service";
+    }
+
 }
 
