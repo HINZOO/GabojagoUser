427e02dc9dcf44cd20cfcbb0f14f58af327914f4
yang <thddns11@naver.com>
2023-04-18 16:45:32 +0900

    service

--- src/main/java/com/project/gabojago/gabojagouser/controller/my/MyUserController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/my/MyUserController.java
@@ -1,19 +1,15 @@
 package com.project.gabojago.gabojagouser.controller.my;
 
-import com.project.gabojago.gabojagouser.dto.comm.CommunityDto;
 import com.project.gabojago.gabojagouser.dto.my.MyUserQnaDto;
 import com.project.gabojago.gabojagouser.dto.user.UserDto;
 import com.project.gabojago.gabojagouser.service.my.MyUserQnaService;
 import com.project.gabojago.gabojagouser.service.user.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -30,15 +26,31 @@ public class MyUserController {
         model.addAttribute("user",loginUser);
         return "/my/user";
     }
-    @GetMapping("/{uId}/service.do")
-    private String serviceForm(Model model,
-                       @ModelAttribute MyUserQnaDto myUserQna,
-                        @SessionAttribute UserDto loginUser){
+    @GetMapping("/{uId}/serviceList.do")
+    private String serviceList(Model model,
+                               @ModelAttribute MyUserQnaDto myUserQna,
+                               @SessionAttribute UserDto loginUser,
+                               @PathVariable String uId){
 
         List<MyUserQnaDto> list= myUserQnaService.list();
         model.addAttribute("qnaList",list);
+        return "/my/serviceList";
+    }
+    @GetMapping ("/service.do")
+    private String serviceForm(Model model,
+                               @SessionAttribute(required = false) UserDto loginUser,
+                               RedirectAttributes redirectAttributes) {
+        if (loginUser == null) {
+            String msg = "로그인한 사용자만 이용할 수 있습니다.";
+            redirectAttributes.addFlashAttribute("msg", msg);
+            return "redirect:/user/login.do";
+        }
         return "/my/service";
     }
 
+
+
+
 }
+
 
