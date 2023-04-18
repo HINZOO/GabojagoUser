b2dab22b4949b1507a952ddbff069804dfb09056
S <s@MacBook-Pro-16.local>
2023-04-18 15:47:26 +0900

    Interceptor Config

--- src/main/java/com/project/gabojago/gabojagouser/interceptorConfig.java
+++ src/main/java/com/project/gabojago/gabojagouser/interceptorConfig.java
@@ -1,4 +1,19 @@
 package com.project.gabojago.gabojagouser;
 
-public class interceptorConfig {
+import com.project.gabojago.gabojagouser.interceptor.AutoLoginInterceptor;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@AllArgsConstructor
+@Configuration
+public class interceptorConfig implements WebMvcConfigurer {
+  private AutoLoginInterceptor autoLoginInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(autoLoginInterceptor)
+            .addPathPatterns("/**");
+  }
 }
