12d0d4cabd1002afbba3ae6a354e3f2e40bb0809
HIMJU <hinge1128@gmail.com>
2023-04-18 14:34:06 +0900

    삭제

---
+++ src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommPageDto.java
@@ -0,0 +1,35 @@
+package com.project.gabojago.gabojagouser.controller.comm;
+
+import lombok.Data;
+
+@Data
+public class CommPageDto {
+    private  int pageNum=1;
+    private int pageSize=5;
+
+    enum CommsType{
+        c_id,u_id,title,view_count,content,post_time,area,
+        istj,istp,isfj,isfp,intj, infp, estj ,estp, esfj, esfp ,entj ,entp,enfp
+    }
+    enum DirecType{
+        DESC,ASC
+    }
+
+    private  CommsType order = CommsType.post_time;
+    private DirecType direct=DirecType.DESC;
+
+    private CommsType searchField;
+    private String searchValue;
+    private String orderBy;
+
+    public String getOrderBy(){
+        if(this.order!=null&& this.direct!=null){
+            return this.order+" "+this.direct;
+        } else if (this.order!=null) {
+            this.direct=DirecType.ASC;
+            return this.order+" "+this.direct;
+        }
+        return CommsType.post_time+" "+DirecType.DESC;
+    }
+
+}
--- src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommReplyController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommReplyController.java
@@ -1,16 +1,13 @@
 package com.project.gabojago.gabojagouser.controller.comm;
 
 import com.project.gabojago.gabojagouser.dto.comm.CommReplyDto;
-import com.project.gabojago.gabojagouser.dto.comm.CommunityDto;
 import com.project.gabojago.gabojagouser.dto.user.UserDto;
 import com.project.gabojago.gabojagouser.service.comm.CommReplyService;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,9 +22,7 @@ public class CommReplyController {
     }
     @Data
     class HandlerDto{
-        private int register=0;
-        private int modify=0;
-        private int remove=0;
+        private int handler=0;
 
     }
     @GetMapping("/{ccId}/detail.do")
@@ -47,33 +42,13 @@ public class CommReplyController {
     public @ResponseBody HandlerDto registerHandler(
             @ModelAttribute CommReplyDto reply,
             @SessionAttribute UserDto loginUser) {
+        String msg="";
         HandlerDto handlerDto = new HandlerDto();
         int register = commReplyService.register(reply);
-        handlerDto.setRegister(register);
+        handlerDto.setHandler(register);
         return handlerDto;
     }
 
-
-    @PostMapping("/insert.do")
-    public String insertAction(
-        @ModelAttribute CommReplyDto reply,
-        RedirectAttributes redirectAttributes,
-        @SessionAttribute UserDto loginUser
-    ){
-        String msg="";
-        int register=0;
-        try{
-            register=commReplyService.register(reply);
-        }catch (Exception e){
-            log.error(e.getMessage());
-            msg="에러"+e.getMessage();
-        }
-        if(register>0){
-            msg="댓글 등록 성공!";
-        }
-        redirectAttributes.addFlashAttribute("msg",msg);
-        return "redirect:/comm/"+reply.getCId()+"/detail.do";
-    }
 
     @PutMapping("/handler.do")
     public @ResponseBody HandlerDto modify(
@@ -84,11 +59,22 @@ public class CommReplyController {
         HandlerDto handlerDto=new HandlerDto();
         try{
             modify=commReplyService.modify(reply);
-            handlerDto.setModify(modify);
+            handlerDto.setHandler(modify);
         }catch (Exception e){
             log.error(e.getMessage());
         }
 
+        return handlerDto;
+    }
+
+    @DeleteMapping("/handler.do?{ccId}")
+    public @ResponseBody HandlerDto remove(
+            CommReplyDto reply,
+            @SessionAttribute UserDto loginUser
+    ){
+        HandlerDto handlerDto=new HandlerDto();
+        int remove=commReplyService.remove(reply.getCcId());
+        handlerDto.setHandler(remove);
         return handlerDto;
     }
 }
