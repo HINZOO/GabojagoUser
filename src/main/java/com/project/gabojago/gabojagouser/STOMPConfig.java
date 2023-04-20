bc0b4666d2a1eab507952c769df6247698305ebe
J-SSS <dongr5102@naver.com>
2023-04-20 23:06:16 +0900

    0420임시저장

--- src/main/java/com/project/gabojago/gabojagouser/STOMPConfig.java
+++ src/main/java/com/project/gabojago/gabojagouser/STOMPConfig.java
@@ -8,10 +8,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class STOMPChatConfig implements WebSocketMessageBrokerConfigurer {
+public class STOMPConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //웹소켓이 없는 클라이 언트와 통신할 때 (websocket, xhr-streaming, xhr-polling 등을 사용해서 통신 시도하는 경로)
         registry.addEndpoint("/stomp/chat")
                 .setAllowedOrigins("http://localhost:7777")
                 .withSockJS();
@@ -21,12 +20,10 @@ public class STOMPChatConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableSimpleBroker("/sub");
-
         /*
         발행(메시지를 서버에 보냄) url의 prefix를 /pub로 지정함
-        메시지 브로커는 받은 메시지를 뿌려주기 위한 url을 enableSimpleBroker에 지정(/sub)
+        메시지를 뿌려주기 위한 url을 enableSimpleBroker에 지정(/sub)하면
+        브로커가 분류해서 뿌려줌
          */
-
-//        //@Controller 에서 @MessageMapping("/hello") /pub/hello 에서 서버로 보낸 메세지를    @SendTo("/sub/greetings") 를 구독하는 클라이언트로 메세지 전달
     }
 }
