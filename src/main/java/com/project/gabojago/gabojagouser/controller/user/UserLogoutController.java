5dca1c98ae8737b4bd1cd7ec5b9b67292754513d
S <s@MacBook-Pro-16.local>
2023-04-20 14:18:20 +0900

    update

--- src/main/java/com/project/gabojago/gabojagouser/controller/user/UserLogoutController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/user/UserLogoutController.java
@@ -1,10 +1,12 @@
 package com.project.gabojago.gabojagouser.controller.user;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,7 +20,19 @@ public class UserLogoutController {
   public String logoutAction(
       HttpSession session,
       RedirectAttributes redirectAttributes,
+      @CookieValue(value = "loginId", required = false) String loginId,
+      @CookieValue(value = "loginPw", required = false) String loginPw,
       HttpServletResponse resp) {
+    if(loginId!=null || loginPw!=null) {
+      Cookie loginIdCookie=new Cookie("loginId", null);
+      Cookie loginPwCookie=new Cookie("loginPw", null);
+      loginIdCookie.setMaxAge(0);
+      loginPwCookie.setMaxAge(0);
+      loginIdCookie.setPath("/");
+      loginPwCookie.setPath("/");
+      resp.addCookie(loginIdCookie);
+      resp.addCookie(loginPwCookie);
+    }
     session.removeAttribute("loginUser");
     redirectAttributes.addFlashAttribute("msg", "로그아웃 성공");
     return "redirect:/";
--- src/main/java/com/project/gabojago/gabojagouser/interceptor/AutoLoginInterceptor.java
+++ src/main/java/com/project/gabojago/gabojagouser/interceptor/AutoLoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.project.gabojago.gabojagouser.interceptor;
 
+import com.project.gabojago.gabojagouser.dto.user.UserDto;
+import com.project.gabojago.gabojagouser.lib.AESEncryption;
 import com.project.gabojago.gabojagouser.service.user.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,25 +14,53 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @AllArgsConstructor
 @Component
 public class AutoLoginInterceptor implements HandlerInterceptor {
+
   private UserService userService;
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     HttpSession session=request.getSession();
     Object loginUser=session.getAttribute("loginUser");
-    if(loginUser!=null) return true;
+    if(loginUser!=null) {
+      return true;
+    }
     Cookie[] cookies=request.getCookies();
-    if(cookies!=null) {
-      String uId=null;
-      String pw=null;
+    if(cookies==null) {
+      return true;
+    }
+      Cookie loginUId=null;
+      Cookie loginPw=null;
       for(Cookie cookie:cookies) {
         if(cookie.getName().equals("loginId")) {
-          uId=cookie.getValue();
+          loginUId=cookie;
         } else if(cookie.getName().equals("loginPw")) {
-          pw=cookie.getValue();
+          loginPw=cookie;
         }
       }
+    if(loginUId!=null && loginPw!=null) {
+      UserDto autoLoginUser=null;
+      try {
+        UserDto user=new UserDto();
+        user.setUId(AESEncryption.decryptValue(loginUId.getValue()));
+        user.setPw(AESEncryption.decryptValue(loginPw.getValue()));
+        autoLoginUser=userService.login(user);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      if(autoLoginUser!=null) {
+        session.setAttribute("loginUser", autoLoginUser);
+        return true;
+      } else {
+        loginUId.setMaxAge(0);
+        loginPw.setMaxAge(0);
+        loginUId.setPath("/");
+        loginPw.setPath("/");
+        response.addCookie(loginUId);
+        response.addCookie(loginPw);
+        response.sendRedirect("/user/login.do");
+        return false;
+      }
     }
-    return HandlerInterceptor.super.preHandle(request, response, handler);
+    return true;
   }
 }
