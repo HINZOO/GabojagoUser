d287a9b7545bff9a49b88d51e4ff940a7b056e60
HIMJU <hinge1128@gmail.com>
2023-04-21 15:21:28 +0900

    Comm_like

--- src/test/java/com/project/gabojago/gabojagouser/mapper/comm/CommLikeMapperTest.java
+++ src/test/java/com/project/gabojago/gabojagouser/mapper/comm/CommLikeMapperTest.java
@@ -2,6 +2,7 @@ package com.project.gabojago.gabojagouser.mapper.comm;
 
 import com.project.gabojago.gabojagouser.dto.comm.CommLikeDto;
 import com.project.gabojago.gabojagouser.dto.comm.LikeStatusCntDto;
+import com.project.gabojago.gabojagouser.mapper.user.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,29 +12,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class CommLikeMapperTest {
     @Autowired
     CommLikeMapper commLikeMapper;
+    @Autowired
+    UserMapper userMapper;
     @Test
-    void findByCIdAndUId() {
+    void findByCIdAndUId() { //해당 유저가 2번글에 좋아요를 했는지 t/f
+        boolean commLike= commLikeMapper.findByCIdAndUId(2,"user01");
+        System.out.println("commLike = " + commLike);
     }
 
     @Test
-    void findByCIdAndUIdIsLoginUserId() {
+    void findByCIdAndUIdIsLoginUserId() { //로그인한 유저가 2번 글에 좋아요 했는지 t/f
+        userMapper.setLoginUserId("user01");
+        boolean commLike=commLikeMapper.findByCIdAndUIdIsLoginUserId(2);
+        userMapper.setLoginUserIdNull();
+        System.out.println("commLike = " + commLike);
+
     }
 
     @Test
     void countStatusByCId() {
+        LikeStatusCntDto likeStatusCntDto=new LikeStatusCntDto();
+        likeStatusCntDto=commLikeMapper.countStatusByCId(2);
+        System.out.println("likeStatusCntDto = " + likeStatusCntDto);
     }
 
     @Test
     void countStatusByUId() {
-    LikeStatusCntDto contUId=commLikeMapper.countStatusByUId("user01");
-    System.out.println("contUId = " + contUId);
+        LikeStatusCntDto likeStatusCntDto= commLikeMapper.countStatusByUId("user02");
+        System.out.println(likeStatusCntDto);
     }
 
     @Test
     void insertOne() {
         CommLikeDto commLikeDto= new CommLikeDto();
         commLikeDto.setCId(2);
-        commLikeDto.setUId("user01");
+        commLikeDto.setUId("user03");
         int insert=commLikeMapper.insertOne(commLikeDto);
         System.out.println("insert = " + insert);
         System.out.println("commLikeDto = " + commLikeDto);
@@ -41,5 +54,9 @@ class CommLikeMapperTest {
 
     @Test
     void deleteOne() {
+        CommLikeDto commLikeDto=new CommLikeDto();
+        commLikeDto.setCId(2);
+        commLikeDto.setUId("user03");
+        commLikeMapper.deleteOne(commLikeDto);
     }
 }
