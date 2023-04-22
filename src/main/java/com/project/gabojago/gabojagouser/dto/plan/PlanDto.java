3baca7ad316ea469b73da2c4a5849a332a2d357d
J-SSS <dongr5102@naver.com>
2023-04-22 16:33:30 +0900

    오류수정

--- src/main/java/com/project/gabojago/gabojagouser/dto/plan/PlanDto.java
+++ src/main/java/com/project/gabojago/gabojagouser/dto/plan/PlanDto.java
@@ -21,5 +21,5 @@ public class PlanDto {
     private int review; // 리뷰작성여부
     private List<PlanCheckListsDto> checkLists; // 체크리스트 데이터
     private List<PlanContentsDto> contents; // 개별 스케쥴 데이터
-    private List<PlanMembersDto> mUsers; // 여행 동반자 유저 정보
+    private List<UserDto> mUsers; // 여행 동반자 유저 정보
 }
