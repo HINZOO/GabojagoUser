089cfc2338eff1cf972129976891d3f5b00cff06
mlulucky <dmswjdans@gmail.com>
2023-04-24 10:13:01 +0900

    병합준비

--- src/main/java/com/project/gabojago/gabojagouser/controller/trip/TripReviewCmtController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/trip/TripReviewCmtController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/reviewCmt")
+@RequestMapping("/reviewcmt")
 @Log4j2
 public class TripReviewCmtController {
     private TripReviewCmtService tripReviewCmtService;
