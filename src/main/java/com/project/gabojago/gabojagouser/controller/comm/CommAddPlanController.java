39c397d523e34b0e3687ef9e4f2651bb53e431c0
HIMJU <hinge1128@gmail.com>
2023-05-01 10:16:36 +0900

    내일정바로가기

--- src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommAddPlanController.java
+++ src/main/java/com/project/gabojago/gabojagouser/controller/comm/CommAddPlanController.java
@@ -3,10 +3,12 @@ package com.project.gabojago.gabojagouser.controller.comm;
 import com.project.gabojago.gabojagouser.dto.plan.PlanContentsDto;
 import com.project.gabojago.gabojagouser.dto.plan.PlanDto;
 import com.project.gabojago.gabojagouser.dto.user.UserDto;
+import com.project.gabojago.gabojagouser.service.plan.PlanContentsService;
 import com.project.gabojago.gabojagouser.service.plan.PlanService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 public class CommAddPlanController {
     private PlanService planService;
+    private PlanContentsService planContentsService;
     @Data
     class HandlerDto{
         private int handler;
@@ -23,22 +26,31 @@ public class CommAddPlanController {
     @PostMapping("/plan/{pId}/handler.do")
     public HandlerDto registerPlanAdd(
             @PathVariable int pId,
-            @SessionAttribute UserDto loginUser){
+            @SessionAttribute UserDto loginUser,
+            RedirectAttributes redirectAttributes){
         int register=0;
+        PlanDto pullPlan=planService.detail(pId);
         PlanDto planDto=new PlanDto();
-        planDto.setTitle(planDto.getTitle());
+        planDto.setTitle(pullPlan.getTitle());
+        planDto.setPlanTo(pullPlan.getPlanTo());
+        planDto.setPlanFrom(pullPlan.getPlanFrom());
+        planDto.setPlanStatus("PUBLIC");
         planDto.setUId(loginUser.getUId());
         planDto.setNkName(loginUser.getNkName());
-
-//        List<PlanContentsDto> newContents=null;
-//        for(PlanContentsDto p:planContents){
-//            PlanContentsDto newTitle=new PlanContentsDto();
-//            newTitle.setTitle(p.getTitle());
-//            newTitle.setTime(p.getTime());
-//            newContents.add(newTitle);
-//        }
-//        planDto.setContents(newContents);
         register= planService.register(planDto);
+        if(register>0){
+            redirectAttributes.addAttribute("msg","등록성공!");
+        }else{
+            redirectAttributes.addAttribute("msg","등록실패!");
+
+        }
+        for(PlanContentsDto p:pullPlan.getContents()){
+            PlanContentsDto pullTitle=new PlanContentsDto();
+            pullTitle.setPId(planDto.getPId());
+            pullTitle.setTitle(p.getTitle());
+            pullTitle.setTime(p.getTime());
+            register= planContentsService.register(pullTitle);
+        }
         HandlerDto handlerDto=new HandlerDto();
         handlerDto.setHandler(register);
         return handlerDto;
